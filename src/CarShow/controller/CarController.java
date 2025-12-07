c09f19580bd1320b671befc6c7965e7759106b5e
ItokianaIZ07 <itokianarabarivelo@gmail.com>
2025-12-07 15:27:06 +0300

    fonctionnalite

--- src/CarShow/controller/CarController.java
+++ src/CarShow/controller/CarController.java
@@ -2,6 +2,7 @@ package CarShow.controller;
 
 import CarShow.model.Car;
 import CarShow.model.ModelCar;
+import CarShow.model.Proprietaire;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -75,9 +76,77 @@ public class CarController {
         return count;
     }
 
-    // filtre car par marque
-    //            par numéro
-    // supprimer car
-    // count car
-    // count à partir marque/modèle
+    public int countCar(Object param){
+        if(param == null){
+            throw new RuntimeException("null value for param in countCar(param)");
+        }
+        int count = 0;
+        if(param instanceof ModelCar){
+            for(Car car:ListCar()){
+                if(car.getModel() != null &&  car.getModel().equals(param)){
+                    count++;
+                }
+            }
+        }else{
+            for(Car car:ListCar()){
+                if(car.getMarque()!= null && car.getMarque().equals(param.toString())){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public String detail(int id){
+        String detail = null;
+        for(Car car: Cars){
+            if(car.getId() == id){
+                detail = car.toString();
+            }
+        }
+        return detail;
+    }
+
+    public Car findByNum(String num){
+        Car carFound = null;
+        for(Car car: Cars){
+            if(car.getNumero().equals(num)){
+                carFound = car;
+            }
+        }
+        return carFound;
+    }
+
+    public List<Car> findByMarque(String marque){
+        List<Car> cars = new ArrayList<>();
+        for(Car car: Cars){
+            if(car.getMarque().equals(marque)){
+                cars.add(car);
+            }
+        }
+
+        return cars;
+    }
+
+    public void deleteCarById(int id){
+        for(Car car: Cars){
+            if(car.getId() == id){
+                Cars.remove(car);
+            }
+        }
+    }
+
+    public void addProp(int idCar, int propId){
+        List<Proprietaire> listProp = ProprietaireController.getListProp();
+        for(Proprietaire prop : listProp){
+            if(prop.getId() == propId){
+                List<Car> listCar = prop.getListVoiture();
+                for(Car car: Cars){
+                    if(car.getId() == idCar){
+                        listCar.add(car);
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ src/CarShow/model/Proprietaire.java
@@ -0,0 +1,50 @@
+package CarShow.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Proprietaire {
+    private int id;
+    private String nom;
+    private String prenom;
+    private List<Car> listVoiture;
+
+    public Proprietaire(int id, String nom, String prenom, List<Car> listVoiture) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        setListVoiture(listVoiture);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public List<Car> getListVoiture() {
+        return listVoiture;
+    }
+
+    public void setListVoiture(List<Car> listVoiture) {
+        this.listVoiture = listVoiture == null ? new ArrayList<>() : listVoiture;
+    }
+}
